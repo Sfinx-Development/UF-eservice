4826e0a92bbb5d6c3eab373bbb0a741d172bfd6e
Angelina Holmqvist <angelinaholmqvist@live.se>
2024-11-23 20:24:22 +0100

    multiline description

--- src/PAGES/Profile.tsx
+++ src/PAGES/Profile.tsx
@@ -180,6 +180,7 @@ export default function ProfilePage() {
           <TextField
             label="Beskrivning"
             value={desc}
+            multiline
             onChange={handleChange(setDesc)}
             fullWidth
           />
